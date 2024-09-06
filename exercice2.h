27e46691ea9e1aef173ff95cce667679c8723eed
safidi1 <167799316+safidi1@users.noreply.github.com>
2024-09-06 20:55:44 +0200

    adresse ip decoupage

---
+++ exercice2.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+
+void verification(int num1,int num2,int num3,int num4);
+void val(int *num1,int *num2,int *num3,int *num4,int *mask);
+void binaire(int *num,int *bite);
+void msr(int *mask1,int *mask);
+void dismsr(int *mask1);
+void adressereseau(int *adr1,int *mask1,int *bite);
+void point(void);
+void inverse(int *mask1);
+void broadcast(int *mask1,int *broad,int *bite);
+void disbroadcast(int *broad);
+void nombrededecoup(int *mask);
+void nbr(int *mask,int *sousreseau);
