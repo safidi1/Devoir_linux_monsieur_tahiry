d1f1aa5a15eb29b0e62c214314411275dec4690c
safidi1 <167799316+safidi1@users.noreply.github.com>
2024-09-06 18:20:58 +0200

    Create exercice1.h
    les prototypes

---
+++ exercice1.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void verification(int num1,int num2,int num3,int num4);
+void val(int *num1,int *num2,int *num3,int *num4,int *mask);
+void binaire(int *num,int *bite);
+void msr(int *mask1,int *mask);
+void dismsr(int *mask1);
+void adressereseau(int *adr1,int *mask1,int *bite);
+void point(void);
+void inverse(int *mask1);
+void broadcast(int *mask1,int *broad,int *bite);
+void disbroadcast(int *broad);
+
